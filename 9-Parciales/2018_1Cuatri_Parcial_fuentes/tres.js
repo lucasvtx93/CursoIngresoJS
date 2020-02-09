faab2b2c815f24732e7bfe6f01bd4eee32d988ff
lucasvtx93 <60628941+lucasvtx93@users.noreply.github.com>
2020-02-09 19:34:13 -0300

    Ejercicio realizado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var resultado;
 
+	precio=parseInt(prompt("Ingrese el precio"));
+	descuento=parseInt(prompt("Ingrese el descuento a aplicar"));
+
+	resultado=precio - (precio * descuento/100);
+
+	document.getElementById('elPrecioFinal').value=resultado;
+	
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
 
+	ancho=parseInt(prompt(" Ingrese el ancho "));
+	largo=parseInt(prompt(" Ingrese el largo "));
+
+	perimetro=2*(ancho+largo);
+
+	alert(" El perimetro es " +perimetro);
 }
