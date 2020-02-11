0246ef2fd57789f3c185bc24aea21d367e8c2d70
lucasvtx93 <60628941+lucasvtx93@users.noreply.github.com>
2020-02-11 17:33:29 -0300

    no muestra nada

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,13 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	var estadocivil;
 
-	
+	edad=document.getElementById('edad').value;
+	estadocivil=document.getElementById('estadoCivil').value;
 
-
+	if(edad>18 && estadocivil=="soltero")
+	{
+		alert("es soltero y es mayor");
+	}
 }//FIN DE LA FUNCIÓN
